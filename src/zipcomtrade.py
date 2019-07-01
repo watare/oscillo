bdc7d7c6e0614f72434f29116e31a87277831dfd
watare <theophile.nathan@gmail.com>
2019-07-01 19:03:49 +0200

    zip+suppression+classementdansdossierparheure

---
+++ src/zipcomtrade.py
@@ -0,0 +1,49 @@
+import json
+import sys
+
+import time
+
+import glob, os
+import os.path
+
+import zipfile
+
+homedir = os.path.expanduser("~")
+chemindesfichiers = homedir+"/oscillo/data"
+
+#initialisation
+
+directoryByHourReference = time.strftime("%m-%d-%H")
+nomCheminDataParHeure = chemindesfichiers+"/"+directoryByHourReference
+print(nomCheminDataParHeure)
+try:
+    os.mkdir(nomCheminDataParHeure)
+except :
+    pass
+
+while True:
+    directoryByHour = time.strftime("%m-%d-%H")
+
+    if directoryByHour != directoryByHourReference:
+        nomCheminDataParHeure = chemindesfichiers+"/"+directoryByHour
+        try:
+            os.mkdir(nomCheminDataParHeure)
+        except :
+            pass
+
+    tempsDernierFichier = int(time.strftime("%M"))-1
+
+    if tempsDernierFichier < 10:
+        tempsDernierFichier = "0"+str(tempsDernierFichier)
+
+    nomFichierPrededent = "trace-"+time.strftime("%m-%d-%H")+"-"+str(tempsDernierFichier)+".pcap"
+    nomFichierCourant = "trace-"+time.strftime("%m-%d-%H-%M")+".pcap"
+
+    for file in glob.glob("/var/tmp/*.pcap"):
+        if (os.path.basename(file) !=nomFichierCourant and os.path.basename(file) !=nomFichierPrededent):
+            zip_file = zipfile.ZipFile(nomCheminDataParHeure+"/"+os.path.basename(file)+'.zip','w')
+            zip_file.write(file, compress_type=zipfile.ZIP_DEFLATED)
+            zip_file.close()
+            os.remove(file)
+
+    time.sleep(30)
