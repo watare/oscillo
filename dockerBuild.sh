653ab480ae37c53cd010b526ee1608c9be1fd131
watare <aurelien.watare@gmail.com>
2019-10-08 16:52:38 +0200

    ensemble des blocs sauf le ftp sont conteneurisiés

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -3,5 +3,6 @@
 #constuction des  images
 docker build -f Dockerfile_zip -t zipcomtrade:latest .
 docker build -f Dockerfile_tcp -t tcpdump:latest .
-
+docker build -f Dockerfile_node -t nodeserver:latest .
+docker build -f Dockerfile_angular -t oscillo-app:latest .
 #sudo docker exec  --mount type=tmpfs,destination=/var/tmp --cap-add sys_nice --cap-add net_admin -it tcpdump:latest
--- lancement_oscillo.sh
+++ lancement_oscillo.sh
@@ -8,27 +8,69 @@ echo initialisation efface les anciens conteneurs et le macvlan
 tcpcontainer="$(docker ps -a| grep 'tcpdump')" &&
 zipcontainer="$(docker ps -a|grep 'zipcomtrade')" &&
 macvlannet="$(docker network ls | grep 'macvlan')" &&
+nodeservercontainer="$(docker ps -a|grep 'nodeserver')" &&
+oscilloappcontainer="$(docker ps -a|grep 'oscillo-app')" &&
 
+echo "suppression $tcpcontainer"
 [ ! -z "$tcpcontainer" ] && \
 docker stop tcpdump && \
-docker container rm tcpdump &&
+docker container rm tcpdump 
 
+echo "suppression $nodeservercontainer"
+[ ! -z "$nodeservercontainer" ] && \
+docker stop nodeserver && \
+docker container rm nodeserver
+
+echo "suppression $zipcontainer"
 [ ! -z "$zipcontainer" ] && \
 docker stop zipcomtrade && \
-docker container rm zipcomtrade &&
+docker container rm zipcomtrade 
+
+echo "suppression $oscilloappcontainer"
+
+[ ! -z "$oscilloappcontainer" ] && \
+docker stop oscillo-app && \
+docker container rm oscillo-app 
+
+echo "suppression $macvlannet"
 
 [ ! -z "$macvlannet" ] && \
-docker network rm macvlan &&
+docker network rm macvlan 
 
 
 #parametre reseau############################################################
-echo configuraiton du macvlan
-# ip="$(ip addr | grep -A 1 $carte_reseau| grep 'inet' | cut -d 'b' -f 1|cut -d 't' -f 2)" &&\
-docker network create -d macvlan -o parent=$carte_reseau macvlan &&
+echo configuraiton du macvlan &&
+ip="$(ip addr | grep -A 1 $carte_reseau|sed 's: ::g' | grep 'inet' | cut -d 'b' -f 1|cut -d 't' -f 2)" &&
+echo $ip
+#docker network create -d macvlan --subnet=$ip -o parent=$carte_reseau macvlan &&
+docker network create -d macvlan --subnet=$ip  -o parent=$carte_reseau macvlan &&
+echo "$carte_reseau"
 #creation et lancement########################################################
 #lancement des  containeurs
 echo lancement des conteneurs
 
+# nodeserver
+sudo docker run \
+--restart always \
+--name=nodeserver \
+-v /oscillo_data:/root/data \
+-v /home/ftpuser:/root/ftp \
+--cap-add sys_nice \
+--cap-add net_admin \
+--net macvlan  \
+nodeserver:latest &
+
+echo "nodeserver lance"
+
+#dockerbuild
+
+sudo docker run \
+--name=oscillo-app \
+--net macvlan \
+oscillo-app:latest &
+
+
+echo "oscillo-app lance"
 
 sudo docker run \
 --restart always \
@@ -39,7 +81,7 @@ sudo docker run \
 --net macvlan  \
 tcpdump:latest &
 
-echo tcpdump lance
+echo "tcpdump lance"
 
 sudo docker run  \
 --restart always \
@@ -51,6 +93,6 @@ sudo docker run  \
 --cap-add net_admin \
 zipcomtrade:latest & disown
 
-echo zipcomtrade lance
+echo "zipcomtrade lance"
 #./dockerRun.sh
 #& disown
